9e6997028c4c7e45fb4c5a3741288298495e86a6
Harang <dbd02169@naver.com>
2024-05-25 01:31:33 +0900

    chore: api response, request 타입 세팅 (#13)

--- frontend/src/lib/apis/scenario.ts
+++ frontend/src/lib/apis/scenario.ts
@@ -1,9 +1,12 @@
+import { ScenariosTickerResponse, ScenarioTickerRequest } from '../types/scenario';
+
 import api from '.';
 
-export const getTicker = async () => {
-  const response = await api<any>({
+export const getTicker = async (params: ScenarioTickerRequest) => {
+  const response = await api<ScenariosTickerResponse>({
     url: '/v1/scenario/{ticker}',
     method: 'POST',
+    params,
   });
 
   return response;
---
+++ frontend/src/lib/types/earningRate.ts
@@ -0,0 +1,5 @@
+export interface EarningRate {
+  asset: number;
+  result: number;
+  rate: string;
+}
