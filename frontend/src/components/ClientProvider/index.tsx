99a362397850fb71feb92e50e8607d3614cebe35
Harang <dbd02169@naver.com>
2024-05-25 00:59:47 +0900

    feat: api setting with react-query (#12)
    * feat: api setting with react-query
    
    * feat: earningRate api 적용

--- frontend/src/components/ClientProvider/index.tsx
+++ frontend/src/components/ClientProvider/index.tsx
@@ -1,14 +1,26 @@
 'use client';
 
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useState } from 'react';
 
 import { NextUIProvider } from '@nextui-org/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function ClientProver({ children }: PropsWithChildren) {
+  const [queryClient] = useState(() => new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+        refetchOnWindowFocus: false,
+      },
+    },
+  }));
+
   return (
-    <NextUIProvider>
-      {children}
-    </NextUIProvider>
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider>
+        {children}
+      </NextUIProvider>
+    </QueryClientProvider>
   );
 }
 
